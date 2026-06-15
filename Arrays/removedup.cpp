6cd15d953f73bec3cd41983e5d9d4c5d78924042
Prem <premmalge121@gmail.com>
2026-06-15 23:04:44 +0530

    Array problem

---
+++ Arrays/removedup.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int removeDuplicates(vector<int>& nums) {
+    if (nums.empty()) return 0;
+
+    int i = 0;
+
+    for (int j = 1; j < nums.size(); j++) {
+        if (nums[j] != nums[i]) {
+            nums[++i] = nums[j];
+        }
+    }
+
+    return i + 1;
+}
+
+int main() {
+    vector<int> nums = {1, 1, 2, 2, 3, 4, 4, 5};
+
+    int k = removeDuplicates(nums);
+
+    for (int i = 0; i < k; i++) {
+        cout << nums[i] << " ";
+    }
+
+    return 0;
+}
